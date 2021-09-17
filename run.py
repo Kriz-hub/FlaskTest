deda4de6555d4dbe5b2201050e0d6ddff2d521f5
Kriz-hub <k.m.dikkeschei@casema.nl>
2021-09-17 11:54:41 +0000

    Initial Commit

---
+++ run.py
@@ -0,0 +1,17 @@
+import os
+from flask import Flask
+
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def index():
+    return "Hello World!"
+
+
+if __name__ == "--main--":
+    app.run(
+        host=os.environ.get("IP", "0.0.0.0"),
+        port=int(os.environ.get("PORT", "5000")),
+        debug=True)
