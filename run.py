993f2e8a61ae650ac4483f15ce44fec060524206
Kriz-hub <k.m.dikkeschei@casema.nl>
2021-09-25 14:17:22 +0000

    basic page made

--- run.py
+++ run.py
@@ -7,10 +7,10 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return "Hello World!"
+    return "Hello, World"
 
 
-if __name__ == "--main--":
+if __name__ == "__main__":
     app.run(
         host=os.environ.get("IP", "0.0.0.0"),
         port=int(os.environ.get("PORT", "5000")),
